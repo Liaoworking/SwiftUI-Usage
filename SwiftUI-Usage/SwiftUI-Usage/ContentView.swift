4c65552c85ff66ef7a535817d762a5bb74c9b8f5
LKIN <runliao@Runs-MacBook-Pro.local>
2019-08-28 00:35:19 +0800

    First Commit

---
+++ SwiftUI-Usage/SwiftUI-Usage/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  SwiftUI-Usage
+//
+//  Created by Run Liao on 2019/8/28.
+//  Copyright © 2019 Run Liao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.red]
+    }
+    
+    var body: some View {
+        NavigationView {
+            
+            List{
+                Text("First Commit")
+            }.navigationBarTitle(Text("Demo")
+                , displayMode: .inline)
+            
+        }
+
+
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
