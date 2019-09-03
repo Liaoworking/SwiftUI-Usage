11eef70f93bdbcf1bddb2fa66e6848b25219de9c
LKIN <runliao@Runs-MacBook-Pro.local>
2019-09-04 00:58:37 +0800

    TextFIeld to be done

--- SwiftUI-Usage/SwiftUI-Usage/ContentViewModel.swift
+++ SwiftUI-Usage/SwiftUI-Usage/ContentViewModel.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct ContentViewModel {
     let modelArray = [ListModel(id: 0,title: "UILabel -> Text", nextView: AnyView( TextView() )),
                     ListModel(id: 1, title: "UIImage -> Image", nextView: AnyView( ImageView() )),
-                    ListModel(id: 2, title: "UIbutton -> Button", nextView: AnyView( ButtonView() ))]
+                    ListModel(id: 2, title: "UIbutton -> Button", nextView: AnyView( ButtonView() )),
+                    ListModel(id: 3, title: "UITextField -> TextField", nextView: AnyView( TextFieldDemo() ))]
 }
 
 struct ListModel: Identifiable {
---
+++ SwiftUI-Usage/SwiftUI-Usage/UsageView/TextFieldDemo.swift
@@ -0,0 +1,81 @@
+//
+//  TextFieldDemo.swift
+//  SwiftUI-Usage
+//
+//  Created by Run Liao on 2019/9/3.
+//  Copyright © 2019 Run Liao. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+struct TextFieldDemo: View {
+    
+    @State var text: String = ""
+    @State var numberKeyBoardText: String = "numberKeyBoardTextHere"
+    // TODO: max length to be done
+    @State var maxLengthText: String = "textFieldMaxLength=20" {
+        willSet {
+            print("触发")
+            self.maxLengthText = String(newValue.prefix(20))
+        }
+        didSet {
+            print("触发333")
+
+        }
+    }
+
+    
+    var body: some View {
+                
+    return GeometryReader { geometry in
+        VStack(alignment: .leading, spacing: 10) {
+            TextField("PlaceHolder Here", text: self.$numberKeyBoardText, onEditingChanged: { (isChange) in
+                print("isChange\(isChange)")
+
+            }) {
+                print("Finished editing")
+            }.keyboardType(.numberPad)
+            .padding()
+            .textFieldStyle(PlainTextFieldStyle())
+            .border(Color.red, width: 1)
+        
+
+            TextField("PlaceHolder Here", text: self.$maxLengthText).textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundColor(Color.red)
+
+            // There is no api for change placeHolder color here, you can use this trick way 😁
+            ZStack(alignment: .leading) {
+                TextField("", text: self.$text)
+                    .background(Color.white)
+                if self.text.isEmpty {
+                    Text("RedColor_Placeholder")
+                        .foregroundColor(.red)
+                }
+            }
+
+            Text("Tap the blank space to hide the keyboard")
+
+        }.frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
+            .background(Color.green)
+        }.gesture(
+            TapGesture().onEnded { (_) in
+                print("tap event")
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
+                // or you can use this way to hide the keyboard
+                //UIApplication.shared.keyWindow?.endEditing(true)
+            }
+        )
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+#if DEBUG
+struct TextFieldDemo_Preview: PreviewProvider {
+    static var previews: TextFieldDemo {
+        return TextFieldDemo()
+    }
+}
+#endif
+
